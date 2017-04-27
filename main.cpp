52da75a4fa7d2e9f841417c8524bc799cc3f0e26
Paul Constantin <paul.2.constantin@continental-corporation.com>
2017-04-27 10:26:57 +0300

    main.cpp cleanup

--- main.cpp
+++ main.cpp
@@ -9,31 +9,6 @@
 int main(int argc, char *argv[])
 {
     std::cout << "Fuck you,world!" << std::endl;
-//    //time to do some random stuff
-//    //creating class objects as I go along to test implementetions
-//    //Messages Class - used to practice class constructors usage
-//    messages rudeMessage; //TODO - use parameters
-//    rudeMessage.getMessage();
-//    rudeMessage.setMessage("hardcoding Style");
-//    rudeMessage.getMessage();
-//    //MessageCentral b;  Done to ilustrate that abstract classes cannot be instantiated
-//    //in common tongue this means you cannot create an object of an abstract class
-//    //MessageCentral objectOfAbstractClass;
-
-//    CartoonMessages mickeyMouse;
-
-//    /* mickeyMouse object created at first with just one pure virtual method
-//     * implemented in derived CartoonMessages Class to validate
-//     * that we cannot instantiate an abstract class,all pure virtual methods
-//     * need reimplementation.
-//     * Will use same object to test after doing propper implementation of derived class
-//     */
-
-//    mickeyMouse.startMessage();
-//    mickeyMouse.endMessage();
-//    mickeyMouse.mainMessage();
-
-//    //TODO move some of these if not most of them into a class made for testing
 
     TestOnly object;
     std::string mata = "ana a ramas fara mere";
