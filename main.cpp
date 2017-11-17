10ffcc45c7354b3094308d58297194a2fec9a010
Paul Constantin <paul.2.constantin@continental-corporation.com>
2017-11-17 14:00:11 +0200

    continuation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "messagecentral.h"
 #include "cartoonmessages.h"
 #include "testonly.h"
+#include <fstream>
+#include <string>
 
 //using namespace std;
 
@@ -11,12 +13,10 @@ T add(T a,T b)
 {
     return a + b;
 }
-
-
 int main(int argc, char *argv[])
 {
     std::cout << "Fuck you,world!" << std::endl;
-
+    std::string myFileName = "Batman.txt";
     TestOnly object;
     std::string mata = "ana a ramas fara mere";
     object.userMessage(mata);
@@ -29,5 +29,20 @@ int main(int argc, char *argv[])
     int a = 1,b = 1;
     int c = add(a,b);
     std::cout << "template function result: " << c << std::endl;
+
+    std::ofstream myFile(myFileName);
+
+    if (!myFile.is_open())
+    {
+         std::cout << "OMFG!!!" << std::endl;
+    } else {
+        myFile.open(myFileName);
+
+    }
+
+    myFile << "Acum punem ceva!";
+    myFile.close();
+
+
     return 0;
 }
