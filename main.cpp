83740c4c22a876ceafb554d1a4ab362a8ff73bbf
Paul Constantin <paul.2.constantin@continental-corporation.com>
2017-12-11 15:00:39 +0200

    threading

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "testonly.h"
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <pthread.h>
 
 //using namespace std;
 
@@ -13,35 +17,58 @@ T add(T a,T b)
 {
     return a + b;
 }
+
+void print()
+{
+    for(int i = 0; i<= 100; i++)
+    {
+        std::cout<< "From t1: "<< i << std::endl;
+    }
+}
 int main(int argc, char *argv[])
 {
-    std::cout << "Fuck you,world!" << std::endl;
-    std::string myFileName = "Batman.txt";
-    TestOnly object;
-    std::string mata = "ana a ramas fara mere";
-    object.userMessage(mata);
-    messages mainMessage;
-    mainMessage.startMessage();
+//    std::cout << "Fuck you,world!" << std::endl;
+//    std::string myFileName = "Batman.txt";
+//    TestOnly object;
+//    std::string mata = "ana a ramas fara mere";
+//    object.userMessage(mata);
+//    messages mainMessage;
+//    mainMessage.startMessage();
 
-    //In order to access static class methods the correct accest is the namespace one i.e. "::"
-    messages::classMessage();
+//    //In order to access static class methods the correct accest is the namespace one i.e. "::"
+//    messages::classMessage();
 
-    int a = 1,b = 1;
-    int c = add(a,b);
-    std::cout << "template function result: " << c << std::endl;
+//    int a = 1,b = 1;
+//    int c = add(a,b);
+//    std::cout << "template function result: " << c << std::endl;
 
-    std::ofstream myFile(myFileName);
+//    std::ofstream myFile(myFileName);
 
-    if (!myFile.is_open())
-    {
-         std::cout << "OMFG!!!" << std::endl;
-    } else {
-        myFile.open(myFileName);
+//    if (!myFile.is_open())
+//    {
+//        std::cout << "OMFG!!!" << std::endl;
+//    } else {
+//        myFile.open(myFileName);
 
-    }
+//    }
 
-    myFile << "Acum punem ceva!";
-    myFile.close();
+//    myFile << "Acum punem ceva!";
+//    myFile.close();
+
+//    int total = 0;
+
+//    // std::vector<int> values = {0 ,1 ,2 ,3 ,4 ,5 };
+//    std::vector<int> nums{3, 4, 2, 8, 15, 267};
+
+//    nums.push_back(2);
+
+//    //auto print = [&](const int& n) { total += n; };
+//    std::for_each(nums.begin(), nums.end(), [&](const int& n) { total += n; });
+//    std::cout<< "total is: "<<total<<std::endl;
+
+
+    std::thread t1(print);
+    t1.join();
 
 
     return 0;
