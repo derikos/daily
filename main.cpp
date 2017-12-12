67dc80bb8bb3f5eb1a6793fcdbe0c1d189413640
Paul Constantin <paul.2.constantin@continental-corporation.com>
2017-12-12 10:16:24 +0200

    threading work continued

--- main.cpp
+++ main.cpp
@@ -18,57 +18,57 @@ T add(T a,T b)
     return a + b;
 }
 
+void printAgain()
+{
+    for(int i = 0;i < 10;i++)
+    {
+        std::cout<<"From t2: "<<i<<std::endl;
+    }
+}
+
 void print()
 {
     for(int i = 0; i<= 100; i++)
     {
         std::cout<< "From t1: "<< i << std::endl;
     }
+    std::thread t2(printAgain);
+    t2.join();
 }
-int main(int argc, char *argv[])
+int main()
 {
-//    std::cout << "Fuck you,world!" << std::endl;
-//    std::string myFileName = "Batman.txt";
-//    TestOnly object;
-//    std::string mata = "ana a ramas fara mere";
-//    object.userMessage(mata);
-//    messages mainMessage;
-//    mainMessage.startMessage();
+    std::cout << "Fuck you,world!" << std::endl;
+    TestOnly object;
+    std::string mata = "ana a ramas fara mere";
+    object.userMessage(mata);
+    messages mainMessage;
+    mainMessage.startMessage();
 
-//    //In order to access static class methods the correct accest is the namespace one i.e. "::"
-//    messages::classMessage();
+    //In order to access static class methods the correct accest is the namespace one i.e. "::"
+    messages::classMessage();
 
-//    int a = 1,b = 1;
-//    int c = add(a,b);
-//    std::cout << "template function result: " << c << std::endl;
+    int a = 1,b = 1;
+    int c = add(a,b);
+    std::cout << "template function result: " << c << std::endl;
 
-//    std::ofstream myFile(myFileName);
+    int total = 0;
 
-//    if (!myFile.is_open())
-//    {
-//        std::cout << "OMFG!!!" << std::endl;
-//    } else {
-//        myFile.open(myFileName);
+    // std::vector<int> values = {0 ,1 ,2 ,3 ,4 ,5 };
+    std::vector<int> nums{3, 4, 2, 8, 15, 267};
 
-//    }
+    nums.push_back(2);
 
-//    myFile << "Acum punem ceva!";
-//    myFile.close();
-
-//    int total = 0;
-
-//    // std::vector<int> values = {0 ,1 ,2 ,3 ,4 ,5 };
-//    std::vector<int> nums{3, 4, 2, 8, 15, 267};
-
-//    nums.push_back(2);
-
-//    //auto print = [&](const int& n) { total += n; };
-//    std::for_each(nums.begin(), nums.end(), [&](const int& n) { total += n; });
-//    std::cout<< "total is: "<<total<<std::endl;
+    //auto print = [&](const int& n) { total += n; };
+    std::for_each(nums.begin(), nums.end(), [&](const int& n) { total += n; });
+    std::cout<< "total is: "<<total<<std::endl;
 
 
     std::thread t1(print);
+//    int numarThreaduri = t1.hardware_concurrency();
     t1.join();
+
+    std::cout<<"t1.hardware_concurrency(): "<< t1.hardware_concurrency() <<std::endl;
+
 
 
     return 0;
